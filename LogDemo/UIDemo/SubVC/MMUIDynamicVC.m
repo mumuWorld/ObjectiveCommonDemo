aefa98926dab4778cb4fc2ad66b04562af6c43ee
mumu <617958070@qq.com>
2020-08-06 00:10:04 +0800

    重力行为

--- LogDemo/UIDemo/SubVC/MMUIDynamicVC.m
+++ LogDemo/UIDemo/SubVC/MMUIDynamicVC.m
@@ -24,17 +24,21 @@
     self.ani = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
 }
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self push];
+    [self gravity];
     [self collision];
+    CGPoint point = [touches.anyObject locationInView:self.view];
+    [self snap:point];
 }
 
 - (void)gravity {
     
     //仿真行为
-    UIGravityBehavior *beha = [[UIGravityBehavior alloc] initWithItems:@[self.img_1,self.img_2]];
+    UIGravityBehavior *beha = [[UIGravityBehavior alloc] initWithItems:@[self.img_1]];
     //设置重力行为的方向 (数值越大加速度越大)
     beha.gravityDirection = CGVectorMake(10, 10);
     //角度
-    beha.angle = M_PI;
+//    beha.angle = M_PI;
     //速度  角度+ 速度= gravityDirection
     beha.magnitude = 10;;
     
@@ -45,17 +49,32 @@
 - (void)collision {
     //仿真行为
     UICollisionBehavior *beha = [[UICollisionBehavior alloc] initWithItems:@[self.img_1,self.img_2]];
+    //碰撞边界
     beha.translatesReferenceBoundsIntoBoundary = true;
+    // items 只碰撞元素 Boundaries 只碰撞边界 everying 碰撞任何东西
+    beha.collisionMode = UICollisionBehaviorModeItems;
+    //边界内边距
+//    beha.translatesReferenceBoundsIntoBoundary =
+    //添加一个边界
+    [beha addBoundaryWithIdentifier:@"ab" fromPoint:CGPointMake(0, 100) toPoint:CGPointMake(self.view.width, 100)];
+    //设置碰撞监听代理
+    beha.collisionDelegate = self;
     [_ani addBehavior:beha];
 }
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+ 
+- (void)push {
+    //推  Continuous 一直推  Instantaneous 推一次
+    UIPushBehavior *push = [[UIPushBehavior alloc] initWithItems:@[self.img_2] mode:UIPushBehaviorModeContinuous];
+    
+    [_ani addBehavior:push];
 }
-*/
 
+- (void)snap:(CGPoint)point {
+    //如果想要多次捕捉行为 需要移除上次行为
+    [_ani removeAllBehaviors];
+    //创建捕捉行为
+    UISnapBehavior *snap = [[UISnapBehavior alloc] initWithItem:self.img_2 snapToPoint:point];
+    snap.damping = 0.3;
+    [_ani addBehavior:snap];
+}
 @end
